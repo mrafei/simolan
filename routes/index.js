f2956a252eff10116d993933d0b6b49bc298cd81
mrafei <m.rafei96@gmail.com>
2023-07-03 20:28:26 +0330

    test: add uri to response

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express', dbName: process.env.DB_NAME });
+router.get("/", function (req, res, next) {
+  res.json({
+    title: "Express",
+    dbName: process.env.DB_NAME,
+    uri: process.env.MONGODB_URI,
+  });
 });
 
 module.exports = router;
